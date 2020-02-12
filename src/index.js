37f5c5f8b8f9aa42e85f4393dc8ba4c88373d702
min9nim <min9nim@gmail.com>
2020-02-13 06:48:29 +0900

    as esModulle

--- src/index.js
+++ src/index.js
@@ -1,23 +1,27 @@
-module.exports = function intervalCall(interval = 1000, useRootContext = false) {
+Object.defineProperty(exports, '__esModule', { value: true })
+module.exports.default = function intervalCall(
+  interval = 1000,
+  useRootContext = false,
+) {
   let globalElapsed = true
-  return (fn) => {
+  return fn => {
     // interval 시간 안에 다시 호출된 함수 콜은 무시한다
     let elapsed = true
     return function(...args) {
-      if(useRootContext ? !globalElapsed : !elapsed){
+      if (useRootContext ? !globalElapsed : !elapsed) {
         // console.warn((fn.name || 'anonymous function') + ' is canceled by intervalCall')
         return // 마지막 호출 후 제한된 경과시간이 지나지 않은 경우 리턴
       }
-      if(useRootContext){
+      if (useRootContext) {
         globalElapsed = false
-      }else{
+      } else {
         elapsed = false
       }
-      
+
       setTimeout(() => {
-        if(useRootContext){
+        if (useRootContext) {
           globalElapsed = true
-        }else{
+        } else {
           elapsed = true
         }
       }, interval)
